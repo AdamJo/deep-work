0fb734f9b1540ac6185e09137ac8d2938c912007
AdamJo <adamj0101@gmail.com>
2017-03-07 13:02:16 -0800

    fix(theme) add missing hashtag

--- src/components/themes/index.js
+++ src/components/themes/index.js
@@ -2,14 +2,17 @@
 const theme = {};
 
 theme.palette = {
+  // active items
   // 0 = initial color,
   // 1 -> 4 = lighter -> darker
   primary: ['#C59121', '#CFA549', '#CA9B35', '#B4841E', '#A2771C'], // hsla(41%, 71%, 45%, 1);
   secondary: ['#577590', '#758EA4', '#66819A', '#506B83', '#486076'], // hsla(208%, 25%, 45%, 1);
   tertiary: ['#FFC42E', '#FFCE54', '#FFC941', '#E8B32A', '#D1A126'], // hsla(43%, 100%, 59%, 1);
-  danger: ['#BF211E', '#CA4946', 'C43532', '#AE1E1C', '#9D1C19'], // hsla(1%, 73%, 43%, 1);
+  danger: ['#BF211E', '#CA4946', '#C43532', '#AE1E1C', '#9D1C19'], // hsla(1%, 73%, 43%, 1);
   success: ['#3D7527', '#608E4E', '#4E813A', '#386B24', '#326020'], // hsl(103, 50%, 31%)
   greyscale: ['#686868', '#838383', '#757575', '#5F5F5F', '#565656'], // hsla(0%, 0%, 40%, 1);
+
+  //fonts and background
   white: ['#eff3f7', '#dee5e5', '#f5f5f5'], // 	hsl(210, 33%, 95%) - hsl(180, 12%, 88%) -	hsl(0, 0%, 96%)
   black: ['#100c08'], // hsl(30, 33%, 5%)
 };
