8758063e60761545916496e97977a04c446af42d
AdamJo <adamj0101@gmail.com>
2017-03-10 15:55:38 -0800

    refactor(DayChart) move active logs into it's own component

--- src/components/molecules/DayChart/index.js
+++ src/components/molecules/DayChart/index.js
@@ -1,23 +1,18 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import styled from 'styled-components';
 
-import { CellButton } from 'components';
+import { CellButton, HoverWork } from 'components';
 
 // no package and verbose = [...Array(24).keys()];
 // adding lodash Range adds an extra 70kb
 const time = [...Array(24).keys()].map(data => `${data + 1} `);
 const allHours = [...Array(24).keys()].map(data => 0);
+
 const Wrapper = styled.div`
   display: flex;
-  justifyContent: center;
-  flex-direction: column;
-  text-align: center;
-`;
-
-const Time = styled.div`
-  display: flex;
-  flex-direciton: row;
+  flex-direction: row;
   justifyContent: space-between;
+  text-align: center;
 `;
 
 function renderTimeLine(format, hour) {
@@ -39,29 +34,42 @@ function calcTime(index) {
 
 const DayChart = (
   {
-    chart = { format: '12', date: 'Thu Mar 09 2017' },
+    chart={ format: '12', date: 'Thu Mar 09 2017' },
     updateWorkDate,
+    closeWorkHover,
+    openWorkHover,
     ...props
   },
 ) => {
   return (
     <Wrapper {...props}>
-      <Time>
-        {time.map((hours, index) => (
-          <div key={index}>
-            <div>{renderTimeLine(chart.format, hours)}</div>
-            <CellButton
-              hourType={allHours[index]}
-              onClick={() => updateWorkDate(calcTime(index), chart.date)}
-            />
-          </div>
-        ))}
-      </Time>
+      {time.map((hours, index) => (
+        <div key={index}>
+          <div>{renderTimeLine(chart.format, hours)}</div>
+          <HoverWork
+            hidden={chart.workHover}
+          />
+          <CellButton
+            hourType={allHours[index]}
+            onClick={() => updateWorkDate(calcTime(index), chart.date)}
+            onMouseDown={() => openWorkHover()}
+            onMouseUp={() => closeWorkHover()}
+          />
+          <HoverWork
+            hidden={chart.workHover}
+          />
+        </div>
+      ))}
     </Wrapper>
   );
 };
 
-DayChart.propTypes = {};
+DayChart.propTypes = {
+  chart: PropTypes.object,
+  updateWorkDate: PropTypes.func,
+  closeWorkHover: PropTypes.func,
+  openWorkHover: PropTypes.func,
+};
 
 DayChart.defaultProps = {};
 
