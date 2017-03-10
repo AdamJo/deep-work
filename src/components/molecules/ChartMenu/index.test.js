6430da71807d80681a57479968dc7cba365c8773
AdamJo <adamj0101@gmail.com>
2017-03-09 17:08:54 -0800

    fix(tests)

--- src/components/molecules/ChartMenu/index.test.js
+++ src/components/molecules/ChartMenu/index.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ChartMenu from '.';
 
-const wrap = (props = {}) => shallow(<ChartMenu {...props} />);
+const wrap = (props = {chart: {viewType: 'day'}}) => shallow(<ChartMenu {...props} />);
 
 it('renders props when passed in', () => {
-  const wrapper = wrap({ id: 'foo' });
-  expect(wrapper.find({ id: 'foo' })).toHaveLength(1);
+  const wrapper = wrap({id: 'foo'});
+  expect(wrapper.find({id: 'foo'})).toHaveLength(1);
 });
