e023b05455d6db03f61e2d755035fbdf84e2c645
AdamJo <adamj0101@gmail.com>
2017-03-13 15:52:36 -0700

    feature(DayChart) add minimal way to add hours

--- src/components/molecules/DayChart/index.js
+++ src/components/molecules/DayChart/index.js
@@ -6,13 +6,19 @@ import { CellButton, HoverWork } from 'components';
 // no package and verbose = [...Array(24).keys()];
 // adding lodash Range adds an extra 70kb
 const time = [...Array(24).keys()].map(data => `${data + 1} `);
-const allHours = [...Array(24).keys()].map(data => 0);
+const allHours = [...Array(48).keys()].map(data => 0);
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
   justifyContent: space-between;
+  flex-wrap: wrap;
   text-align: center;
+  user-select: none;
+`;
+
+const Hours = styled.div`
+  user-select: none;
 `;
 
 function renderTimeLine(format, hour) {
@@ -27,10 +33,27 @@ function renderTimeLine(format, hour) {
   }
 }
 
-function calcTime(index) {
-  allHours[index] = allHours[index] === 0 ? 1 : 0;
+function deepOrShallow(index) {
+  switch (index) {
+    case 0:
+      return 1;
+    case 1:
+      return 2;
+    case 2:
+      return 0;
+  }
+}
+
+function calcTime(index, half) {
+  allHours[index] = deepOrShallow(allHours[index]);
   return allHours;
 }
+
+function findWork(index) {
+  allHours[index] = deepOrShallow(allHours[index]);
+}
+
+
 
 const DayChart = (
   {
@@ -42,22 +65,23 @@ const DayChart = (
   },
 ) => {
   return (
-    <Wrapper {...props}>
+    <Wrapper {...props}
+    >
+      {/*onMouseDown={() => findWork()}*/}
       {time.map((hours, index) => (
         <div key={index}>
-          <div>{renderTimeLine(chart.format, hours)}</div>
-          <HoverWork
-            hidden={chart.workHover}
-          />
-          <CellButton
-            hourType={allHours[index]}
-            onClick={() => updateWorkDate(calcTime(index), chart.date)}
-            onMouseDown={() => openWorkHover()}
-            onMouseUp={() => closeWorkHover()}
-          />
-          <HoverWork
-            hidden={chart.workHover}
-          />
+          <Hours >{renderTimeLine(chart.format, hours)}</Hours>
+          <div style={{display: 'flex'}} >
+            <CellButton
+              hourType={allHours[index * 2]}
+              onClick={() => updateWorkDate(calcTime(index * 2), chart.date)}
+            ></CellButton>
+              &nbsp;
+            <CellButton
+              hourType={allHours[index * 2 + 1]}
+              onClick={() => updateWorkDate(calcTime(index * 2 + 1), chart.date)}
+            ></CellButton>
+          </div>
         </div>
       ))}
     </Wrapper>
