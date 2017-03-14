a692638efaed5858833f412d56f65c11afcc3919
AdamJo <adamj0101@gmail.com>
2017-03-14 15:59:15 -0700

    chore(AtomTemplate) add PropTyles and styled theme

--- src/components/atoms/AtomTemplate/index.js
+++ src/components/atoms/AtomTemplate/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import styled from 'styled-components';
+import { font, palette } from 'styled-theme';
 
 const AtomTemplate = styled.div`
 `;
