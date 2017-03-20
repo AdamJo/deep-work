e456243bb5132ca62bf8475f5d58a5caef70edd2
AdamJo <adamj0101@gmail.com>
2017-03-20 16:07:19 -0700

    chore(debug) add menu option debug for toggle

--- src/components/organisms/Header/index.js
+++ src/components/organisms/Header/index.js
@@ -1,17 +1,18 @@
 import React, { PropTypes, PureComponent } from 'react';
 import styled from 'styled-components';
 
-import { Button, TimeFormat } from 'components';
+import { Button, TimeFormat, MenuOptions } from 'components';
 
 const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   padding: 1rem;
+  height: 60px;
 `;
 
 const ButtonWrapper = styled.div`
-  width: 600px;
+  min-width: 600px;
   display: flex;
   justify-content: center;
 `
@@ -23,6 +24,7 @@ class Header extends PureComponent {
     this.logIn = this.logIn.bind(this);
     this.getInfo = this.getInfo.bind(this);
     this.setInfo = this.setInfo.bind(this);
+    this.openMenu = this.openMenu.bind(this);
   }
 
   logOut() {
@@ -42,21 +44,28 @@ class Header extends PureComponent {
     this.props.setUserInfo(path, this.props.chart);
   }
 
+  openMenu() {
+    this.props.toggleMenu();
+  }
+
   renderUserMenu(currentUser) {
     if (currentUser.finished) {
       return (
         <ButtonWrapper>
           {currentUser && currentUser.uid
-            ? <div>
+            ?
+            <div>
                 <Button onClick={this.logOut}>Logout</Button>
                 {/*<Button onClick={this.getInfo}>getInfo</Button>
                 <Button onClick={this.setInfo}>setInfo</Button>*/}
               </div>
-            : <Button onClick={this.logIn}>LogIn</Button>}
+            : <div><Button onClick={this.logIn}>LogIn</Button></div>
+          }
           <TimeFormat
             updateTimeFormat={this.props.updateTimeFormat}
             format={this.props.chart.format === '12' ? true : false}
           />
+
         </ButtonWrapper>
       );
     } else {
