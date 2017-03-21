4d0a1d1aee1f5c0783ea29b36035d1f73a2541a9
AdamJo <adamj0101@gmail.com>
2017-03-20 21:25:14 -0700

    feature(Modal) WIP use react modal and arc to create a small menu
    Added css to only display a small amount instead of full page.
    
    Needs more styling for now.

--- src/components/molecules/Modal/index.js
+++ src/components/molecules/Modal/index.js
@@ -11,22 +11,55 @@ const Wrapper = styled.div`
   justify-content: center;
 `
 
+const ModalBox = styled(ReactModal)`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  font-family: ${font('primary')};
+  font-size: 1rem;
+  background-color: ${palette('grayscale', 0, true)};
+  border-radius: 0.125em;
+  color: ${palette('grayscale', 0)};
+  top: calc(20% - 1rem);
+  left: calc(50% - 1rem);
+  right: auto;
+  bottom: auto;
+  margin: 1rem calc(-50% + 1rem) 1rem 1rem;
+  transform: translate(-50%, 100%);
+  transition: transform 250ms ease-in-out;
+  outline: none;
+  box-sizing: border-box;
+  min-width: 320px;
+  max-width: calc(640px - 1rem);
+  max-height: calc(100% - 1rem);
+  padding-top: ${({ hasHeader }) => hasHeader ? 0 : '1rem'};
+  @media screen and (max-width: 640px) {
+    width: calc(100% - 1rem);
+    min-width: 0;
+  }
+  &[class*="after-open"] {
+    transform: translate(-50%, -50%);
+  }
+  &[class*="before-close"] {
+    transform: translate(-50%, 100%);
+  }
+`
+
 const Modal = ({isOpen, ...props}) => {
   console.log(props);
   return (
     <Wrapper>
-      <ReactModal 
+      <ModalBox 
         isOpen={isOpen}
         onRequestClose={props.toggleMenu}
         contentLabel="Settings"
-
       >
         <button onClick={props.toggleMenu}>Close Modal</button>
         <TimeFormat
           updateTimeFormat={props.updateTimeFormat}
           format={props.chart.format === '12' ? true : false}
         />
-      </ReactModal>
+      </ModalBox>
     </Wrapper>
   )
 }
