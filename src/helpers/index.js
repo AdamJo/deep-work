caea9cc101f47909c4d96caf4f672278f2b4d513
AdamJo <adamj0101@gmail.com>
2017-03-23 12:35:49 -0700

    feature(helpers) create a file that exports functions used in multiple files

---
+++ src/helpers/index.js
@@ -0,0 +1,17 @@
+/**
+ * retrieves total amount hours worked
+ * @param {object} day - work days objects {0: 0, 1:1}
+ * @param {bool} flag - checks if the work is deep or shallow for calculation
+ * @return {number} the total number of work done that day
+ */
+export function getHours(day, flag) {
+  if (day !== undefined) {
+    if (!!Object.keys(day).length) {
+      return flag ? day.deep : day.shallow;
+    } else {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
+};
