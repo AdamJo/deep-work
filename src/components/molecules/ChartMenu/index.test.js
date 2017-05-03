842ff197603afa6804d4eb8436270d81937537f5
AdamJo <adamj0101@gmail.com>
2017-05-03 12:45:48 -0700

    test(ChartMenu) correctly displays a day, week, month button

--- src/components/molecules/ChartMenu/index.test.js
+++ src/components/molecules/ChartMenu/index.test.js
@@ -1,4 +1,5 @@
 import ChartMenu from '.';
+import { Button } from 'components';
 
 const wrap = (props = { chart: { viewType: 'day' } }) =>
   shallow(<ChartMenu {...props} />);
@@ -6,4 +7,11 @@ const wrap = (props = { chart: { viewType: 'day' } }) =>
 it('renders props when passed in', () => {
   const wrapper = wrap({ id: 'foo' });
   expect(wrapper.find({ id: 'foo' })).toHaveLength(1);
+});
+
+it('should contain buttons for day, week, month', () => {
+  const wrapper = wrap();
+  expect(wrapper.contains('day')).toBe(true);
+  expect(wrapper.contains('week')).toBe(true);
+  expect(wrapper.contains('month')).toBe(true);
 });
